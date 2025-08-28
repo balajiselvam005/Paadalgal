b9342192524572f2ec93fb428b7122dfbad2dc4e
muthu <muthuganesh0013@gmail.com>
2025-08-28 15:39:54 +0530

    comments

--- src/main/java/com/paadalgal/Paadalgal/PaadalgalApplication.java
+++ src/main/java/com/paadalgal/Paadalgal/PaadalgalApplication.java
@@ -1,15 +1,11 @@
 package com.paadalgal.Paadalgal;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Set;
-
 @SpringBootApplication
 public class PaadalgalApplication {
-
+//hi
 	public static void main(String[] args) {
 		SpringApplication.run(PaadalgalApplication.class, args);
 	}
