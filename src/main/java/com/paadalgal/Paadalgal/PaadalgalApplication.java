ba6436675f6f650a4b2d48d0ea292949b84727b3
balajiselvam005 <balajiselvam005@gmail.com>
2025-08-28 18:37:51 +0530

    Added Song model
    # Conflicts:
    #	src/main/java/com/paadalgal/Paadalgal/PaadalgalApplication.java

--- src/main/java/com/paadalgal/Paadalgal/PaadalgalApplication.java
+++ src/main/java/com/paadalgal/Paadalgal/PaadalgalApplication.java
@@ -1,37 +1,17 @@
 package com.paadalgal.Paadalgal;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
-
-import com.cloudinary.utils.ObjectUtils;
-
+import com.cloudinary.Cloudinary;
 import io.github.cdimascio.dotenv.Dotenv;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.cloudinary.Cloudinary;
+import java.io.File;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class PaadalgalApplication {
-
-	public static void main(String[] args) throws IOException {
-				Dotenv dotenv=Dotenv.load();
-				dotenv.get("CLOUDINARY_CLOUD_NAME");
-		        Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name",dotenv.get("CLOUDINARY_CLOUD_NAME"),
-                "api_key", dotenv.get("CLOUDINARY_API_KEY"),
-                "api_secret", dotenv.get("CLOUDINARY_API_SECRET")));
-				File folder = new File("M:/Paadalgal/src/assets/audio/");
-        for (File file : folder.listFiles()) {
-            if (file.isFile() && (file.getName().endsWith(".mp3") || file.getName().endsWith(".wav"))) {
-                Map uploadResult = cloudinary.uploader().upload(file, ObjectUtils.asMap(
-                        "resource_type", "auto"
-                ));
-            }
-        }
+	public static void main(String[] args) {
 		SpringApplication.run(PaadalgalApplication.class, args);
 	}
 }
