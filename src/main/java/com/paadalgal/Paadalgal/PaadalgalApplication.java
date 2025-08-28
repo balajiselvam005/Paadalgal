ff7ef7aa3476b2436cf9fdaaadeb8ffdae1df500
balajiselvam005 <balajiselvam005@gmail.com>
2025-08-28 14:56:40 +0530

    First commit

---
+++ src/main/java/com/paadalgal/Paadalgal/PaadalgalApplication.java
@@ -0,0 +1,13 @@
+package com.paadalgal.Paadalgal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaadalgalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaadalgalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paadalgal/Paadalgal/PaadalgalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paadalgal.Paadalgal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaadalgalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
